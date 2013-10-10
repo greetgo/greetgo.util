544b41cc8b941900821b8f3eef9428bf812cbb69
den <den@332b23a8-2734-466c-9918-71a3b56b5c77>
2013-10-10 09:33:13 +0000

    NamedTitle.
    git-svn-id: http://192.168.11.11/svn/greetgo/trunk@76 332b23a8-2734-466c-9918-71a3b56b5c77

---
+++ greetgo.sgwt/src/main/java/kz/greetgo/sgwt/base/NamedTitle.java
@@ -0,0 +1,11 @@
+package kz.greetgo.sgwt.base;
+
+public final class NamedTitle {
+  public final String name;
+  public final String title;
+  
+  private NamedTitle(String name, String title) {
+    this.name = name;
+    this.title = title;
+  }
+}
