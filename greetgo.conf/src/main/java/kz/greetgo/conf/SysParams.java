d516b73a64dbded1df9e3bb6cd2d9f228f09bdcc
pompei <pompei@332b23a8-2734-466c-9918-71a3b56b5c77>
2013-10-10 05:26:41 +0000

    Добавлены параметры для PostgreSQL в SysParams
    git-svn-id: http://192.168.11.11/svn/greetgo/trunk@65 332b23a8-2734-466c-9918-71a3b56b5c77

--- greetgo.conf/src/main/java/kz/greetgo/conf/SysParams.java
+++ greetgo.conf/src/main/java/kz/greetgo/conf/SysParams.java
@@ -3,11 +3,19 @@ package kz.greetgo.conf;
 public class SysParams {
   
   public static String get(String name) {
+    return get(name, null);
+  }
+  
+  public static String get(String name, String defaultValue) {
     {
       String ret = System.getProperty(name);
       if (ret != null && ret.trim().length() > 0) return ret;
     }
-    return System.getenv(name);
+    {
+      String ret = System.getenv(name);
+      if (ret != null && ret.trim().length() > 0) return ret;
+    }
+    return defaultValue;
   }
   
   public static String oracleAdminHost() {
@@ -49,4 +57,29 @@ public class SysParams {
   public static String oracleAdminPasswordKey() {
     return "ORACLE_ADMIN_PASSWORD";
   }
+  
+  public static String pgAdminUrlKey() {
+    return "PG_ADMIN_URL";
+  }
+  
+  public static String pgAdminUrl() {
+    return get(pgAdminUrlKey(), "jdbc:postgresql://localhost/postgres");
+  }
+  
+  public static String pgAdminUseridKey() {
+    return "PG_ADMIN_USERID";
+  }
+  
+  public static String pgAdminUserid() {
+    return get(pgAdminUseridKey(), "postgres");
+  }
+  
+  public static String pgAdminPasswordKey() {
+    return "PG_ADMIN_PASSWORD";
+  }
+  
+  public static String pgAdminPassword() {
+    return get(pgAdminPasswordKey(), "");
+  }
+  
 }
