a27d5cb52b439ceed00bae0abbc86a7ed08542fb
den <den@332b23a8-2734-466c-9918-71a3b56b5c77>
2013-10-11 07:53:46 +0000

    AbstractEntryPoint.
    git-svn-id: http://192.168.11.11/svn/greetgo/trunk@83 332b23a8-2734-466c-9918-71a3b56b5c77

---
+++ greetgo.sgwt/src/main/java/kz/greetgo/sgwt/base/AbstractEntryPoint.java
@@ -0,0 +1,47 @@
+package kz.greetgo.sgwt.base;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.smartgwt.client.types.DateDisplayFormat;
+import com.smartgwt.client.util.DateUtil;
+import com.smartgwt.client.widgets.Canvas;
+
+public abstract class AbstractEntryPoint implements EntryPoint {
+  public final Canvas root;
+  
+  public AbstractEntryPoint(Canvas root) {
+    this.root = root;
+  }
+  
+  public static final boolean debug() {
+    return Window.Location.getParameter("debug") != null;
+  }
+  
+  @Override
+  public final void onModuleLoad() {
+    GWT.setUncaughtExceptionHandler(new GWT.UncaughtExceptionHandler() {
+      @Override
+      public void onUncaughtException(Throwable e) {
+        e.printStackTrace();
+      }
+    });
+    
+    DateUtil.setNormalDateDisplayFormat(DateDisplayFormat.TOEUROPEANSHORTDATE);
+    DateUtil.setShortDateDisplayFormat(DateDisplayFormat.TOEUROPEANSHORTDATE);
+    DateUtil.setShortDatetimeDisplayFormat(DateDisplayFormat.TOEUROPEANSHORTDATETIME);
+    
+    prepare();
+    
+    try {
+      RootPanel.get("loading").setVisible(false);
+    } catch (Exception e) {}
+    
+    root.setWidth100();
+    root.setHeight100();
+    root.draw();
+  }
+  
+  protected abstract void prepare();
+}
