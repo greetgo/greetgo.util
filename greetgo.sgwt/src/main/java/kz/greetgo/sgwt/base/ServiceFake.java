d12ed7837c8873478c1bd6204a42d5bb04cce564
den <den@332b23a8-2734-466c-9918-71a3b56b5c77>
2013-10-11 04:07:17 +0000

    ServiceFake.
    git-svn-id: http://192.168.11.11/svn/greetgo/trunk@80 332b23a8-2734-466c-9918-71a3b56b5c77

---
+++ greetgo.sgwt/src/main/java/kz/greetgo/sgwt/base/ServiceFake.java
@@ -0,0 +1,77 @@
+package kz.greetgo.sgwt.base;
+
+import java.util.ArrayList;
+
+import kz.greetgo.gwtshare.base.ServiceAsync;
+
+import com.google.gwt.regexp.shared.RegExp;
+import com.google.gwt.regexp.shared.SplitResult;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public abstract class ServiceFake<A, R> implements ServiceAsync<A, R> {
+  private static final int[] DEFAULT_DELAYS = new int[] { 500 };
+  private static final RegExp SPLITTER = RegExp.compile("[^0-9]+");
+  
+  private static int[] DELAYS = DEFAULT_DELAYS;
+  private static int SEQ = 0;
+  
+  public static final void setDelays(int... initDelays) {
+    for (int i = 0; i < initDelays.length; i++) {
+      initDelays[i] = Math.max(0, initDelays[i]);
+    }
+    DELAYS = initDelays.length == 0 ? DEFAULT_DELAYS : initDelays;
+  }
+  
+  public static final void setDelays(String delaysStr) {
+    SplitResult strs = SPLITTER.split(delaysStr);
+    ArrayList<Integer> initDelays = new ArrayList<Integer>();
+    for (int i = 0; i < strs.length(); i++) {
+      try {
+        initDelays.add(Integer.parseInt(strs.get(i)));
+      } catch (NumberFormatException e) {}
+    }
+    int[] array = new int[initDelays.size()];
+    for (int i = 0; i < array.length; i++) {
+      array[i] = initDelays.get(i);
+    }
+    setDelays(array);
+  }
+  
+  @Override
+  public final void invoke(final A action, final AsyncCallback<R> callback) {
+    String qualified = this.getClass().toString();
+    final String className = qualified.substring(qualified.lastIndexOf('.') + 1);
+    final int seq = SEQ++;
+    final int currentDelay = DELAYS[seq % DELAYS.length];
+    final boolean trace = !"PingServiceFake".equals(className);
+    new Timer() {
+      {
+        trace("<<", action);
+      }
+      
+      @Override
+      public void run() {
+        R result;
+        try {
+          result = fakeInvoke(action);
+        } catch (Throwable caught) {
+          trace("!!", caught);
+          callback.onFailure(caught);
+          return;
+        }
+        trace(">>", result);
+        callback.onSuccess(result);
+      }
+      
+      private final void trace(String dir, Object obj) {
+        if (trace) {
+          System.out.println("##" + seq + " " + currentDelay + " " + className + " " + dir + " "
+              + obj);
+        }
+      }
+    }.schedule(currentDelay);
+  }
+  
+  protected abstract R fakeInvoke(A action);
+}
