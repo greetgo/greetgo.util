37f3ab0278851d2df17be9c65118be5574e0f883
den <den@332b23a8-2734-466c-9918-71a3b56b5c77>
2013-10-16 11:40:34 +0000

    Разделены Listing и Collecting.
    git-svn-id: http://192.168.11.11/svn/greetgo/trunk@97 332b23a8-2734-466c-9918-71a3b56b5c77

---
+++ greetgo.sgwt/src/main/java/kz/greetgo/sgwt/component/Collecting.java
@@ -0,0 +1,80 @@
+package kz.greetgo.sgwt.component;
+
+import java.util.List;
+
+import kz.greetgo.gwtshare.base.ServiceAsync;
+import kz.greetgo.gwtshare.base.Sync;
+import kz.greetgo.sgwt.base.BaseCallback;
+
+import com.smartgwt.client.types.SelectionStyle;
+import com.smartgwt.client.widgets.IButton;
+import com.smartgwt.client.widgets.events.ClickEvent;
+import com.smartgwt.client.widgets.events.ClickHandler;
+import com.smartgwt.client.widgets.grid.ListGridRecord;
+import com.smartgwt.client.widgets.grid.events.SelectionChangedHandler;
+import com.smartgwt.client.widgets.grid.events.SelectionEvent;
+import com.smartgwt.client.widgets.layout.VLayout;
+
+public abstract class Collecting<T, F> extends Listing<T> {
+  private final ServiceAsync<F, List<T>> listService;
+  public final VLayout root;
+  public F filter;
+  
+  public Collecting(final Saving<T> saving, ServiceAsync<F, List<T>> listService,
+      final ServiceAsync<T, Void> removeService) {
+    this.listService = listService;
+    
+    ClickHandler toSave = new ClickHandler() {
+      public void onClick(ClickEvent event) {
+        boolean isNew = ((IButton)event.getSource()).getTitle().equals(PLUS);
+        saving.invoke(isNew ? null : selection(), new Sync<T>() {
+          public void invoke(T t) {
+            update();
+          }
+        });
+      }
+    };
+    
+    IButton plus = new IButton(PLUS, toSave);
+    final IButton ellipsis = new IButton(ELLIPSIS, toSave);
+    
+    final IButton minus = new IButton(MINUS, new ClickHandler() {
+      public void onClick(ClickEvent event) {
+        removeService.invoke(selection(), new BaseCallback<Void>() {
+          public void onSuccess(Void result) {
+            update();
+          }
+        });
+      }
+    });
+    
+    grid.setSelectionType(SelectionStyle.SINGLE);
+    grid.addSelectionChangedHandler(new SelectionChangedHandler() {
+      public void onSelectionChanged(SelectionEvent event) {
+        boolean disabled = grid.getSelectedRecord() == null;
+        minus.setDisabled(disabled);
+        ellipsis.setDisabled(disabled);
+      }
+    });
+    
+    minus.setDisabled(true);
+    ellipsis.setDisabled(true);
+    
+    plus.setWidth(TOOL);
+    minus.setWidth(TOOL);
+    ellipsis.setWidth(TOOL);
+    
+    root = vl(hl(grid, toolbar(vl(plus, minus, ellipsis))));
+    root.setMembersMargin(GRAIN);
+    root.setWidth100();
+  }
+  
+  public void update() {
+    grid.setData((ListGridRecord[])null);
+    listService.invoke(filter, new BaseCallback<List<T>>() {
+      public void onSuccess(List<T> result) {
+        render(result);
+      }
+    });
+  }
+}
