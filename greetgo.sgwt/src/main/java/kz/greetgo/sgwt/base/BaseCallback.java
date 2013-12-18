569d16f33498bd15448793a43771f71774b43dd8
aidos <aidos@332b23a8-2734-466c-9918-71a3b56b5c77>
2013-12-18 05:45:33 +0000

    С параметром hideerrors все ошибки скрываются.
    git-svn-id: http://192.168.11.11/svn/greetgo/trunk@135 332b23a8-2734-466c-9918-71a3b56b5c77

--- greetgo.sgwt/src/main/java/kz/greetgo/sgwt/base/BaseCallback.java
+++ greetgo.sgwt/src/main/java/kz/greetgo/sgwt/base/BaseCallback.java
@@ -10,9 +10,11 @@ public abstract class BaseCallback<R> implements AsyncCallback<R> {
   
   @Override
   public void onFailure(Throwable caught) {
-    boolean isSafe = caught instanceof ClientException
-        || Window.Location.getParameter("showerrors") != null;
-    SC.warn(isSafe ? caught.getMessage() : "Сбой сервера.");
+    if (Window.Location.getParameter("hideerrors") == null) {
+      boolean isSafe = caught instanceof ClientException
+          || Window.Location.getParameter("showerrors") != null;
+      SC.warn(isSafe ? caught.getMessage() :"Сбой сервера.");
+    }
     caught.printStackTrace();
   }
 }
