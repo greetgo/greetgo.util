93691115c90c4c2f02b2fcfa45006231981b7964
ilyas <ilyas@332b23a8-2734-466c-9918-71a3b56b5c77>
2014-01-22 04:35:52 +0000

    обработали hideerrors: теперь ClientException отображается всегда
    git-svn-id: http://192.168.11.11/svn/greetgo/trunk@147 332b23a8-2734-466c-9918-71a3b56b5c77

--- greetgo.sgwt/src/main/java/kz/greetgo/sgwt/base/BaseCallback.java
+++ greetgo.sgwt/src/main/java/kz/greetgo/sgwt/base/BaseCallback.java
@@ -1,6 +1,7 @@
 package kz.greetgo.sgwt.base;
 
 import kz.greetgo.gwtshare.base.ClientException;
+import kz.greetgo.gwtshare.base.SgwtException;
 
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
@@ -10,11 +11,17 @@ public abstract class BaseCallback<R> implements AsyncCallback<R> {
   
   @Override
   public void onFailure(Throwable caught) {
-    if (Window.Location.getParameter("hideerrors") == null) {
-      boolean isSafe = caught instanceof ClientException
-          || Window.Location.getParameter("showerrors") != null;
-      SC.warn(isSafe ? caught.getMessage() :"Сбой сервера.");
+    if (caught instanceof ClientException) {
+      SC.say(caught.getMessage());
+      return;
     }
+    
+    if (Window.Location.getParameter("hideerrors") == null) {
+      boolean isShow = caught instanceof SgwtException
+          || Window.Location.getParameter("showerrors") != null;
+      if (isShow) SC.warn(caught.getMessage());
+    }
+    
     caught.printStackTrace();
   }
 }
