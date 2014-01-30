b4686245033ff70e6c872eac88ac358d9205cd2e
pompei <pompei@332b23a8-2734-466c-9918-71a3b56b5c77>
2014-01-30 05:54:06 +0000

    Share project "greetgo.HomeCreditDemo" into "http://192.168.0.1/svn/greetgo"
    git-svn-id: http://192.168.11.11/svn/greetgo/trunk@158 332b23a8-2734-466c-9918-71a3b56b5c77

---
+++ greetgo.HomeCreditDemo/src/kz/greetgo/homecreditdemo/KaruselActivity.java
@@ -0,0 +1,22 @@
+package kz.greetgo.homecreditdemo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class KaruselActivity extends Activity {
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_karusel);
+  }
+  
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.karusel, menu);
+    return true;
+  }
+  
+}
---
+++ greetgo.HomeCreditDemo/src/kz/greetgo/homecreditdemo/MainActivity.java
@@ -0,0 +1,58 @@
+package kz.greetgo.homecreditdemo;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    
+    Button enter = (Button)findViewById(R.id.enter);
+    enter.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        enter();
+      }
+    });
+    
+    login = (EditText)findViewById(R.id.login);
+    password = (EditText)findViewById(R.id.password);
+  }
+  
+  private EditText login, password;
+  
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
+  
+  private void enter() {
+    if ("asd".equals(login.getText().toString()) && "111".equals(password.getText().toString())) {
+      next();
+      return;
+    }
+    
+    AlertDialog.Builder b = new AlertDialog.Builder(this);
+    b.setMessage(R.string.wronge_login_password);
+    b.setTitle("Ошибка при входе");
+    b.setCancelable(true);
+    AlertDialog ad = b.create();
+    ad.show();
+  }
+  
+  private void next() {
+    Intent intent = new Intent(this, KaruselActivity.class);
+    startActivity(intent);
+  }
+}
