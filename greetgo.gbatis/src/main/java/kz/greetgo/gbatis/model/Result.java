11b370c61c859ab1e509c05af71c2feb299dfe5d
pompei <pompei@332b23a8-2734-466c-9918-71a3b56b5c77>
2014-11-05 10:28:09 +0000

    Добавлены фабричные методы в класс Result
    git-svn-id: http://192.168.11.11/svn/greetgo/trunk@299 332b23a8-2734-466c-9918-71a3b56b5c77

--- greetgo.gbatis/src/main/java/kz/greetgo/gbatis/model/Result.java
+++ greetgo.gbatis/src/main/java/kz/greetgo/gbatis/model/Result.java
@@ -34,4 +34,34 @@ public class Result {
    * Место вывода sql-ей для трэйсинга (может быть null)
    */
   public SqlViewer sqlViewer = null;
+  
+  public static Result setOf(Class<?> classs) {
+    Result ret = new Result();
+    ret.resultDataClass = classs;
+    ret.type = ResultType.SET;
+    return ret;
+  }
+  
+  public static Result listOf(Class<?> classs) {
+    Result ret = new Result();
+    ret.resultDataClass = classs;
+    ret.type = ResultType.LIST;
+    return ret;
+  }
+  
+  public static Result simple(Class<?> classs) {
+    Result ret = new Result();
+    ret.resultDataClass = classs;
+    ret.type = ResultType.SIMPLE;
+    return ret;
+  }
+  
+  public static Result mapOf(String keyFieldName, Class<?> keyClass, Class<?> valueClass) {
+    Result ret = new Result();
+    ret.resultDataClass = valueClass;
+    ret.type = ResultType.MAP;
+    ret.mapKeyClass = keyClass;
+    ret.mapKeyField = keyFieldName;
+    return ret;
+  }
 }
