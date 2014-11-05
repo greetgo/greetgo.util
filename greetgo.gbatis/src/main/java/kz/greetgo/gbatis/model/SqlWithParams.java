fcc6046f6aa8b65f2b4c073f7caf2b3b917fee9a
pompei <pompei@332b23a8-2734-466c-9918-71a3b56b5c77>
2014-11-05 08:57:39 +0000

    Добавлены фабричные методы в SqlWithParams
    git-svn-id: http://192.168.11.11/svn/greetgo/trunk@296 332b23a8-2734-466c-9918-71a3b56b5c77

--- greetgo.gbatis/src/main/java/kz/greetgo/gbatis/model/SqlWithParams.java
+++ greetgo.gbatis/src/main/java/kz/greetgo/gbatis/model/SqlWithParams.java
@@ -1,10 +1,65 @@
 package kz.greetgo.gbatis.model;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class SqlWithParams {
   public RequestType type;
   public String sql;
   public final List<Object> params = new ArrayList<>();
+  
+  public static SqlWithParams select(String sql, Object... params) {
+    SqlWithParams ret = new SqlWithParams();
+    ret.type = RequestType.Sele;
+    ret.sql = sql;
+    for (Object p : params) {
+      ret.params.add(p);
+    }
+    return ret;
+  }
+  
+  public static SqlWithParams select(String sql, Collection<Object> params) {
+    SqlWithParams ret = new SqlWithParams();
+    ret.type = RequestType.Sele;
+    ret.sql = sql;
+    ret.params.addAll(params);
+    return ret;
+  }
+  
+  public static SqlWithParams call(String sql, Object... params) {
+    SqlWithParams ret = new SqlWithParams();
+    ret.type = RequestType.Call;
+    ret.sql = sql;
+    for (Object p : params) {
+      ret.params.add(p);
+    }
+    return ret;
+  }
+  
+  public static SqlWithParams call(String sql, Collection<Object> params) {
+    SqlWithParams ret = new SqlWithParams();
+    ret.type = RequestType.Call;
+    ret.sql = sql;
+    ret.params.addAll(params);
+    return ret;
+  }
+  
+  public static SqlWithParams modi(String sql, Object... params) {
+    SqlWithParams ret = new SqlWithParams();
+    ret.type = RequestType.Modi;
+    ret.sql = sql;
+    for (Object p : params) {
+      ret.params.add(p);
+    }
+    return ret;
+  }
+  
+  public static SqlWithParams modi(String sql, Collection<Object> params) {
+    SqlWithParams ret = new SqlWithParams();
+    ret.type = RequestType.Modi;
+    ret.sql = sql;
+    ret.params.addAll(params);
+    return ret;
+  }
 }
