d4b2bc35bc03a238d0586237fe0930cfcf4be1ab
pompei <pompei@332b23a8-2734-466c-9918-71a3b56b5c77>
2014-11-10 09:07:14 +0000

    Добавлено детектирование типа БД по DataSource
    git-svn-id: http://192.168.11.11/svn/greetgo/trunk@337 332b23a8-2734-466c-9918-71a3b56b5c77

--- greetgo.util/src/main/java/kz/greetgo/util/db/DbTypeDetector.java
+++ greetgo.util/src/main/java/kz/greetgo/util/db/DbTypeDetector.java
@@ -3,11 +3,22 @@ package kz.greetgo.util.db;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
 public class DbTypeDetector {
   public static DbType detect(Connection con) throws SQLException {
     String db = con.getMetaData().getDatabaseProductName().toLowerCase();
     if ("oracle".equals(db)) return DbType.Oracle;
     if ("postgresql".equals(db)) return DbType.PostgreSQL;
     throw new IllegalArgumentException("Unknown connection db type");
+  }
+  
+  public static DbType detect(DataSource dataSource) throws SQLException {
+    Connection con = dataSource.getConnection();
+    try {
+      return detect(con);
+    } finally {
+      con.close();
+    }
   }
 }
