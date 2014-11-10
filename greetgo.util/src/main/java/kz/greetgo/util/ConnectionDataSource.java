964fb090fb0d7e882204342e67475c19854c3807
pompei <pompei@332b23a8-2734-466c-9918-71a3b56b5c77>
2014-11-10 08:06:33 +0000

    Added ConnectionDataSource
    git-svn-id: http://192.168.11.11/svn/greetgo/trunk@324 332b23a8-2734-466c-9918-71a3b56b5c77

---
+++ greetgo.util/src/main/java/kz/greetgo/util/ConnectionDataSource.java
@@ -0,0 +1,43 @@
+package kz.greetgo.util;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+
+public class ConnectionDataSource extends AbstractDataSource {
+  private final Connection con;
+  
+  public ConnectionDataSource(Connection con) {
+    this.con = con;
+  }
+  
+  @Override
+  public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+    throw new SQLFeatureNotSupportedException();
+  }
+  
+  @Override
+  public Connection getConnection(String username, String password) throws SQLException {
+    throw new UnsupportedOperationException();
+  }
+  
+  @Override
+  public Connection getConnection() throws SQLException {
+    return (Connection)Proxy.newProxyInstance(getClass().getClassLoader(),
+        new Class<?>[] { Connection.class }, new InvocationHandler() {
+          @Override
+          public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            if (method.getName().equals("close") && method.getParameterTypes().length == 0) {
+              //skip closing of connection
+              return null;
+            }
+            return method.invoke(con, args);
+          }
+        });
+  }
+  
+}
