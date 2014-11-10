dc0cfb95131a24e31710f83925a77e137bcffc94
pompei <pompei@332b23a8-2734-466c-9918-71a3b56b5c77>
2014-11-10 08:12:22 +0000

    Обновлены тесты
    git-svn-id: http://192.168.11.11/svn/greetgo/trunk@326 332b23a8-2734-466c-9918-71a3b56b5c77

--- greetgo.gbatis.util/src/test/java/kz/greetgo/gbatis/util/impl/TestingUtilRegister.java
+++ greetgo.gbatis.util/src/test/java/kz/greetgo/gbatis/util/impl/TestingUtilRegister.java
@@ -1,53 +1,14 @@
 package kz.greetgo.gbatis.util.impl;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.SQLFeatureNotSupportedException;
-import java.util.logging.Logger;
 
 import kz.greetgo.gbatis.futurecall.SqlViewer;
+import kz.greetgo.util.ConnectionDataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.AbstractDataSource;
 
 public class TestingUtilRegister extends AbstractUtilRegister {
   
-  private final class ConnectionDataSource extends AbstractDataSource {
-    private final Connection con;
-    
-    private ConnectionDataSource(Connection con) {
-      this.con = con;
-    }
-    
-    @Override
-    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
-      throw new SQLFeatureNotSupportedException();
-    }
-    
-    @Override
-    public Connection getConnection(String username, String password) throws SQLException {
-      throw new UnsupportedOperationException();
-    }
-    
-    @Override
-    public Connection getConnection() throws SQLException {
-      return (Connection)Proxy.newProxyInstance(getClass().getClassLoader(),
-          new Class<?>[] { Connection.class }, new InvocationHandler() {
-            @Override
-            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-              if (method.getName().equals("close") && method.getParameterTypes().length == 0) {
-                //skip closing of connection
-                return null;
-              }
-              return method.invoke(con, args);
-            }
-          });
-    }
-  }
-
   private JdbcTemplate jdbc;
   
   @Override
