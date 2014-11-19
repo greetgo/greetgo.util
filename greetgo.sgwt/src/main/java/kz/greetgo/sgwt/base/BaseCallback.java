0a033f9915de20a0cfe2c0fb987f09f4cd27ce78
ilyas <ilyas@332b23a8-2734-466c-9918-71a3b56b5c77>
2014-11-19 09:30:03 +0000

    Вывод stackTrace
    git-svn-id: http://192.168.11.11/svn/greetgo/trunk@357 332b23a8-2734-466c-9918-71a3b56b5c77

--- greetgo.sgwt/src/main/java/kz/greetgo/sgwt/base/BaseCallback.java
+++ greetgo.sgwt/src/main/java/kz/greetgo/sgwt/base/BaseCallback.java
@@ -1,6 +1,7 @@
 package kz.greetgo.sgwt.base;
 
 import kz.greetgo.gwtshare.base.ClientException;
+import kz.greetgo.gwtshare.base.SgwtException;
 
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
@@ -9,8 +10,12 @@ import com.smartgwt.client.util.SC;
 
 public abstract class BaseCallback<R> implements AsyncCallback<R> {
   
+  private static final String SHOW_ERROR = "showerrors";
+  private static final String SHOW_ALL_ERROR = "showallerrors";
+  
   @Override
   public void onFailure(Throwable caught) {
+    caught.printStackTrace();
     
     if ((caught instanceof StatusCodeException && ((StatusCodeException)caught).getStatusCode() == 401)
         || (caught.getMessage() != null && caught.getMessage().contains("j_spring_security_check"))) {
@@ -18,16 +23,20 @@ public abstract class BaseCallback<R> implements AsyncCallback<R> {
       return;
     }
     
-    if (caught instanceof ClientException) {
-      SC.say(caught.getMessage());
+    if (Window.Location.getParameter(SHOW_ERROR) != null) {
+      if (caught instanceof ClientException || caught instanceof SgwtException) {
+        SC.warn(caught.getClass() + ": " +caught.getMessage());
+        return;
+      }
+    } 
+    
+    if (Window.Location.getParameter(SHOW_ALL_ERROR) != null) {
+      SC.warn(caught.getClass() + ": " +caught.getMessage());
       return;
     }
     
-    if (Window.Location.getParameter("hideerrors") == null) {
-      boolean isSafe = caught instanceof ClientException
-          || Window.Location.getParameter("showerrors") != null;
-      SC.warn(isSafe ? caught.getMessage() :"Сбой сервера.");
+    if (caught instanceof ClientException) {
+      SC.warn(caught.getMessage());
     }
-    caught.printStackTrace();
   }
 }
