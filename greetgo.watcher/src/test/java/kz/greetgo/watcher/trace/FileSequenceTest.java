0d61a7d4654d40402a1870ed9932eb3ad597d23a
Denis Grotsev <denis.grotsev@gmail.com>
2015-02-18 10:14:28 +0600

    Add test FileSequenceTest.rotate_hole.

--- greetgo.watcher/src/test/java/kz/greetgo/watcher/trace/FileSequenceTest.java
+++ greetgo.watcher/src/test/java/kz/greetgo/watcher/trace/FileSequenceTest.java
@@ -11,8 +11,11 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class FileSequenceTest {
-  private static final Iterable<File> TEST = FileSequence.fileSequence(new File("build/tmp"), "p",
+  private static final Iterable<File> FILES = FileSequence.fileSequence(new File("build/tmp"), "p",
       ".txt", 9, 11);
+  private static final File FILE9 = new File("build/tmp/p09.txt");
+  private static final File FILE10 = new File("build/tmp/p10.txt");
+  private static final File FILE11 = new File("build/tmp/p11.txt");
   
   @BeforeTest
   public void prepare() {
@@ -21,25 +24,38 @@ public class FileSequenceTest {
   
   @Test
   public void iterator() {
-    File file9 = new File("build/tmp/p09.txt");
-    File file10 = new File("build/tmp/p10.txt");
-    File file11 = new File("build/tmp/p11.txt");
-    assertThat(TEST).containsExactly(file9, file10, file11);
+    assertThat(FILES).containsExactly(FILE9, FILE10, FILE11);
   }
   
   @Test
   public void rotate() throws IOException {
-    for (File file : TEST) {
+    for (File file : FILES) {
       file.createNewFile();
       PrintStream ps = new PrintStream(file);
       ps.print(file.getName());
       ps.close();
     }
-    FileSequence.rotate(TEST);
-    Iterator<File> it = TEST.iterator();
+    FileSequence.rotate(FILES);
+    Iterator<File> it = FILES.iterator();
     assertThat(it.next()).doesNotExist();
     assertThat(it.next()).exists().hasContent("p09.txt");
     assertThat(it.next()).exists().hasContent("p10.txt");
+  }
+  
+  @Test
+  public void rotate_hole() throws IOException {
+    for (File file : FILES) {
+      file.createNewFile();
+      PrintStream ps = new PrintStream(file);
+      ps.print(file.getName());
+      ps.close();
+    }
+    FILE10.delete();
+    FileSequence.rotate(FILES);
+    Iterator<File> it = FILES.iterator();
+    assertThat(it.next()).doesNotExist();
+    assertThat(it.next()).exists().hasContent("p09.txt");
+    assertThat(it.next()).exists().hasContent("p11.txt");
   }
   
   public static void main(String[] args) throws IOException {
