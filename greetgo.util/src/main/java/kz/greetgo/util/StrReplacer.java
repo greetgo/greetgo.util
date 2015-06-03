58aa1cdbbfad0a4554ae55be05fb79911039a836
pompei <ekolpakov@greet-go.com>
2015-06-03 13:06:57 +0600

    Расширен фассад StrReplacer

--- greetgo.util/src/main/java/kz/greetgo/util/StrReplacer.java
+++ greetgo.util/src/main/java/kz/greetgo/util/StrReplacer.java
@@ -8,13 +8,53 @@ public class StrReplacer {
   
   private final Map<String, String> paramMap = new HashMap<String, String>();
   
-  public StrReplacer add(String s) {
+  public StrReplacer add(CharSequence s) {
     sb.append(s);
     return this;
   }
   
-  public StrReplacer a(int i) {
+  public StrReplacer add(CharSequence s, int start, int end) {
+    sb.append(s, start, end);
+    return this;
+  }
+  
+  public StrReplacer add(int i) {
     sb.append(i);
+    return this;
+  }
+  
+  public StrReplacer add(char[] str, int offset, int len) {
+    sb.append(str, offset, len);
+    return this;
+  }
+  
+  public StrReplacer add(char[] str) {
+    sb.append(str);
+    return this;
+  }
+  
+  public StrReplacer add(Integer i) {
+    sb.append(i);
+    return this;
+  }
+  
+  public StrReplacer add(long l) {
+    sb.append(l);
+    return this;
+  }
+  
+  public StrReplacer add(Long l) {
+    sb.append(l);
+    return this;
+  }
+  
+  public StrReplacer add(StringBuilder sb) {
+    sb.append(sb);
+    return this;
+  }
+  
+  public StrReplacer add(StrReplacer sr) {
+    sb.append(sr.sb);
     return this;
   }
   
@@ -36,10 +76,10 @@ public class StrReplacer {
       String key = s.substring(i1 + 1, i2);
       String value = paramMap.get(key);
       if (value == null) {
-        ret.append(s.substring(0, i2));
+        ret.append(s, 0, i2);
         s = s.substring(i2);
       } else {
-        ret.append(s.substring(0, i1)).append(value);
+        ret.append(s, 0, i1).append(value);
         s = s.substring(i2 + 1);
       }
     }
