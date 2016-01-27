5ef5c07d027e76acdb658d31e7a51dbc9d0be3e6
ekolpakov <ekolpakov@greet-go.com>
2016-01-27 10:05:58 +0600

    v0.0.12: added class RND

---
+++ greetgo.util/src/main/java/kz/greetgo/util/RND.java
@@ -0,0 +1,68 @@
+package kz.greetgo.util;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Random;
+
+public class RND {
+  
+  public static final String rus = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+  public static final String RUS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+  
+  public static final String eng = "abcdefghijklmnopqrstuvwxyz";
+  public static final String ENG = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  
+  public static final String DEG = "0123456789";
+  
+  public static final String ALL = rus + RUS + eng + ENG + DEG;
+  
+  public static final Random rnd = new Random();
+  
+  public static String str(int len) {
+    char[] charArray = new char[len];
+    for (int i = 0; i < len; i++) {
+      charArray[i] = ALL.charAt(rnd.nextInt(ALL.length()));
+    }
+    return new String(charArray);
+  }
+  
+  public static String intStr(int len) {
+    char[] charArray = new char[len];
+    for (int i = 0; i < len; i++) {
+      charArray[i] = DEG.charAt(rnd.nextInt(DEG.length()));
+    }
+    return new String(charArray);
+  }
+  
+  public static long plusLong(long max) {
+    long L = rnd.nextLong();
+    if (L < 0) L = -L;
+    return L % max;
+  }
+  
+  public static int plusInt(int max) {
+    return rnd.nextInt(max);
+  }
+  
+  public static Date dateYears(int yearFrom, int yearTo) {
+    Calendar cal = new GregorianCalendar();
+    cal.add(Calendar.YEAR, yearFrom);
+    long from = cal.getTimeInMillis();
+    cal.add(Calendar.YEAR, yearTo - yearFrom);
+    long to = cal.getTimeInMillis();
+    if (from > to) {
+      long tmp = from;
+      from = to;
+      to = tmp;
+    }
+    final long time = from + plusLong(to - from);
+    return new Date(time);
+  }
+  
+  public static byte[] byteArray(int len) {
+    final byte[] ret = new byte[len];
+    rnd.nextBytes(ret);
+    return ret;
+  }
+}
